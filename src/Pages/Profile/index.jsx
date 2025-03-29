e0be614a953e91fe64b3695dc0af9a5fa03df171
Luizynhoo <lhenrique1804@gmail.com>
2025-03-29 00:22:36 -0300

    FrontPageProfile

--- src/Pages/Profile/index.jsx
+++ src/Pages/Profile/index.jsx
@@ -2,17 +2,19 @@ import { useContext, useState } from 'react'
 
 import Header from '../../components/Header/header'
 import Title from '../../components/Title'
-import {AuthContext} from '../../contexts/auth'
+import { AuthContext } from '../../contexts/auth'
 
 import { FiSettings, FiUpload } from 'react-icons/fi'
 import avatar from '../../assets/avatar.png'
+
+import './profile.css'
 
 export default function Profile() {
 
     const { user } = useContext(AuthContext);
 
     //Parei aqui 4:22
-    const [avatarUrl, setAvatarUrl] = useState (user && user.avatarUrl)
+    const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl)
 
 
     return (
@@ -24,18 +26,42 @@ export default function Profile() {
                 <Title name="Meu Perfil">
                     <FiSettings size={25} color="#f8f8f8" />
                 </Title>
-            </div>
 
-            <div className='container'>
-                <form className='form-profile'>
-                    <label className='label-avatar'>
-                        <label>
-                            <FiUpload color='#f8f8f8' size={25}/>
+
+                <div className='container'>
+
+                    <form className='form-profile'>
+                        <label className='label-avatar'>
+                            <span>
+                                <FiUpload color='#f8f8f8' size={25} />
+                            </span>
+
+
+                            <input type="file" accept='image/*' />
+                            {/* Se o usuario não tiver foto = avatarGenerico se não a foto dele mesmo */}
+                            {avatarUrl === null ? (
+                                <img src={avatar} alt='foto de perfil' width={250} height={250} />
+                            ) : (
+                                <img src={avatarUrl} alt='foto de perfil' width={250} height={250} />
+                            )}
+
                         </label>
-                    <input type="file" accept='image/*'/>
 
-                    </label>
-                </form>
+                        <label>Nome</label>
+                        <input type="text" placeholder='Seu nome' />
+
+                        <label>E-mail</label>
+                        <input type="text" placeholder='teste@email.com' disabled={true} />
+
+                        <button className='btnP' type='submit'>Salvar</button>
+                    </form>
+
+                </div>
+
+                <div className='container'>
+                    <button className='Logout-btn'>Sair</button>
+                </div>
+
             </div>
         </div>
     )
